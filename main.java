874444c5f48539604be86b91d6dfebee339f6b27
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2024-09-18 14:35:19 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+    public static PrintStream out = System.out;
+    public static void main(String[] args) {
+        out.println("Hello!");
+    }
+}
