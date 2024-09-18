5356716e0a6fb1b5163ee8fa2f3fbcf6ff4c59b3
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2024-09-18 15:35:50 +0300

    Update main.java

--- main.java
+++ main.java
@@ -1,9 +1,99 @@
-import java.io.PrintStream;
 import java.util.Scanner;
-public class Main {
-    public static Scanner in = new Scanner(System.in);
-    public static PrintStream out = System.out;
-    public static void main(String[] args) {
-        out.println("Hello!");
+class asf{
+    int x;
+    public asf(int x){
+        this.x = x;
+    }
+    public int touch_grass(){
+        return this.x;
+    }
+    public static int af(asf x, asf y){
+        return x.touch_grass() + y.touch_grass();
+    }
+    public static int asl(asf x, asf y){
+        return x.touch_grass() - y.touch_grass();
+    }
+    public static int ate(asf x, asf y){
+        return x.touch_grass() * y.touch_grass();
+    }
+    public static int banger(asf x, asf y){
+        return x.touch_grass() / y.touch_grass();
+    }
+    public static int basic(asf x, asf y){
+        return x.touch_grass()%y.touch_grass();
+    }
+}
+class cringe{
+    String x;
+    public cringe(String x){
+        this.x = x;
+    }
+    public String touch_grass(){
+        return this.x;
+    }
+    public static String based(cringe x, cringe y){
+        return x.touch_grass() + y.touch_grass();
+    }
+    public static String bde(cringe x, asf y){
+        String e = "";
+        for(int i = 0; i<y.touch_grass(); i++) e+=x.touch_grass();
+        return e;
+    }
+    public static cringe ded(asf x){
+        return new cringe(String.valueOf(x.touch_grass()));
+    }
+    public static cringe ded(rizz x){
+        return new cringe(String.valueOf(x.touch_grass()));
+    }
+}
+class rizz{
+    char x;
+    public rizz(char x){
+        this.x = x;
+    }
+    public char touch_grass(){
+        return this.x;
+    }
+    public String bestie(){
+        return "" + this.x;
+    }
+}
+class blud{
+    boolean x;
+    public blud(boolean x){
+        this.x = x;
+    }
+    public boolean touch_grass(){
+        return x;
+    }
+}
+class drip{
+    double x;
+    public drip(double x){
+        this.x = x;
+    }
+    public double touch_grass(){
+        return this.x;
+    }
+}
+class brainrot{
+    Scanner in = new Scanner(System.in); 
+    public static int cap(){
+        return in.nextInt();
+    }
+    public static double dogs(){
+        return in.nextDouble();
+    }
+    public static String cook(){
+        return in.nextLine();
+    }
+    public static char cooked(){
+        return in.next.charAt(0);
+    }
+    public static void clapback(cringe x){
+        System.out.prinln(x.touch_grass());
+    }
+    public static void dab(cringe x){
+        System.out.print(x.touch_grass());
     }
 }
