6282b5c2afdd10b6e4655ec869de5c166cfeb4e3
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2024-09-19 23:08:51 +0300

    Was it always this bad?

---
+++ example.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        brainrot skibidi = new brainrot();
+        asf x = new asf(skibidi.cap());
+        asf e = new asf(skibidi.cap());
+        ohio x0 = new ohio(x);
+        x0.fire(e);
+        skibidi.clapback(cringe.ded(new asf(x0.ghost())));
+        x0.skull_emoji();
+        x0.glowup();
+    }
+}
+
+//пример использования
+//оставим на домашнее изучение что вообще тут происходит
+//можете попробовать в компиляторе лол
