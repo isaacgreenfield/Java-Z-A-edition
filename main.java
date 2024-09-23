ff9f928216ae03189e4ca1aa9280d9222c089b9d
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2024-09-23 20:30:10 +0300

    Update main.java

--- main.java
+++ main.java
@@ -41,7 +41,7 @@ class circus{
     public static void clown(){
         Random r = new Random();
         while(true == true){
-            int n = r.nextInt(100000);
+            int n = r.nextInt(1000);
             if(n == 0) break;
         }
     }
